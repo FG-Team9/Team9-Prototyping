8ec07dbcb3c3c754e395361e1efd28650b073d9b
MarinBruh <71761793+MarinBruh@users.noreply.github.com>
2022-10-05 15:13:53 +0200

    aaaaaaaaaaaaaaaaaaaa

---
+++ SnowmanGame/Assets/Scripts/Enemy/EnemyHealth.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+    public int startingHealth = 100;
+    public int scoreValue = 10;
+    
+    ParticleSystem hitParticles;
+    CapsuleCollider capsuleCollider;
+    bool isDead;
+    int currentHealth;
+
+    public int CurrentHealth { get { return currentHealth; } }
+
+    void Awake ()
+    {
+        hitParticles = GetComponentInChildren <ParticleSystem> ();
+        capsuleCollider = GetComponent <CapsuleCollider> ();
+
+        currentHealth = startingHealth;
+    }
+
+
+    void Update ()
+    {
+        
+    }
+
+
+    public void TakeDamage (int amount, Vector3 hitPoint)
+    {
+        if(isDead) return;
+
+        currentHealth -= amount;
+            
+        hitParticles.transform.position = hitPoint;
+        hitParticles.Play();
+
+        if(currentHealth <= 0)
+        {
+            Death ();
+        }
+    }
+
+
+    void Death ()
+    {
+        isDead = true;
+        
+        GetComponent <UnityEngine.AI.NavMeshAgent> ().enabled = false;
+        GetComponent <Rigidbody> ().isKinematic = true;
+        
+        capsuleCollider.isTrigger = true;
+
+        ScoreManager.score += scoreValue;
+        Destroy (gameObject, 2f);
+        Debug.Log("is dead now thanks");
+    }
+}
---
+++ SnowmanGame/Assets/Scripts/Managers/PauseManager.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+public class PauseManager : MonoBehaviour 
+{
+	
+	Canvas canvas;
+	
+	void Start()
+	{
+		canvas = GetComponent<Canvas>();
+	}
+	
+	void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.Escape))
+		{
+			canvas.enabled = !canvas.enabled;
+			Pause();
+		}
+	}
+	
+	public void Pause()
+	{
+		Time.timeScale = Time.timeScale == 0 ? 1 : 0;
+	}
+
+	public void MainMenu()
+	{
+		SceneManager.LoadScene("MainMenu");
+		//Time.timeScale == 1;
+	}
+	
+	public void Quit()
+	{
+		#if UNITY_EDITOR 
+		EditorApplication.isPlaying = false;
+		#else 
+		Application.Quit();
+		#endif
+	}
+}
